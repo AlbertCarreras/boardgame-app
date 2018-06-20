dbfab77bf26958c5f89667775f1b245c614824c0
Alberto Carreras <a.carreras.c@gmail.com>
2018-06-20 12:15:51 -0400

    add unregister to event if registered on event show page

--- app/controllers/event_players_controller.rb
+++ app/controllers/event_players_controller.rb
@@ -7,27 +7,27 @@ class EventPlayersController < ApplicationController
     redirect_back(fallback_location: root_path)
   end
 
-  # def destroy
-  #   if current_user_ownership?
-  #     find_ownership.delete
-  #   end
-  #   redirect_back(fallback_location: root_path)
-  # end
-  #
+  def destroy
+    if current_user_event_player?
+      find_event_player.delete
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
   private
-  #
-  # def current_user_ownership?
-  #   current_user.ownership_ids.include?(find_ownership_id)
-  # end
-  #
-  # def find_ownership_id
-  #   Ownership.find(params[:ownership][:ownership_id].to_i).id
-  # end
-  #
-  # def find_ownership
-  #   Ownership.find(params[:ownership][:ownership_id].to_i)
-  # end
-  #
+
+  def current_user_event_player?
+    current_user.event_player_ids.include?(find_event_player_id)
+  end
+
+  def find_event_player_id
+    EventPlayer.find(params[:event_player][:event_player_id].to_i).id
+  end
+
+  def find_event_player
+    EventPlayer.find(params[:event_player][:event_player_id].to_i)
+  end
+
   def eventplayer_params
     params.require(:event_player).permit(:event_id)
   end
--- app/helpers/event_player_helper.rb
+++ app/helpers/event_player_helper.rb
@@ -1,10 +1,18 @@
 module EventPlayerHelper
 
+  def current_event_player(event_id)
+    current_user.event_players.find_by(event_id: event_id)
+  end
+
+  def current_event_player_id(event_id)
+    current_event_player(event_id).id
+  end
+
   def player_list_by_player(event_id)
     Event.find(event_id).players
   end
 
-  def includes_current_player?(event_id)
+  def registered?(event_id)
     player_list_by_player(event_id).include?(current_user)
   end
 
@@ -15,7 +23,7 @@ module EventPlayerHelper
   def show_count_prompt(event_id)
     a = player_list_count(event_id)
     prompt = pluralize(a, 'registered player')
-    if includes_current_player?(event_id)
+    if registered?(event_id)
       link_to 'Unregister', event_path(event_id)
     else
       if a == 0
