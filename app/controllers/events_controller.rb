7ed18a2a7d47254822bb5683f946d8af335c9154
Alberto Carreras <a.carreras.c@gmail.com>
2018-06-21 13:16:24 -0400

    add more fields to event and game and user

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -60,7 +60,7 @@ class EventsController < ApplicationController
   end
 
   def event_params
-    params.require(:event).permit(:title, :location, :date)
+    params.require(:event).permit(:title, :description, :address, :city, :country, :date)
   end
 
 end
